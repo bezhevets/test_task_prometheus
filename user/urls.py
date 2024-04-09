3242e32ece4bd7e65ff32669e08797c96e143db0
Maksym Bezhevets <begik4@gmail.com>
2024-04-09 21:53:48 +0300

    Added login(token) endpoint (url, serializer, view)

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, CreateTokenView
 
-urlpatterns = [path("register/", CreateUserView.as_view(), name="register")]
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("login/", CreateTokenView.as_view(), name="login"),
+]
 
 app_name = "user"
