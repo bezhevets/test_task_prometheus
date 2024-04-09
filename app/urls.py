7e5efb7765296e3e4440a03dfe08220bc790fa2f
Maksym Bezhevets <begik4@gmail.com>
2024-04-09 21:32:36 +0300

    Added CreateUserView and url register

--- app/urls.py
+++ app/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/post/", include("post.urls", namespace="post")),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
