5cb9a003e0f6c32794cb3ed623977b3cd9bcfbbd
Maksym Bezhevets <begik4@gmail.com>
2024-04-09 23:05:38 +0300

    Added model Like

--- post/models.py
+++ post/models.py
@@ -14,3 +14,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class Like(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="likes"
+    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    created_at = models.DateTimeField(auto_now_add=True)
