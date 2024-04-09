1194364b870b2ba8b44fbcf8a5399001166e3b2f
Maksym Bezhevets <begik4@gmail.com>
2024-04-09 19:55:39 +0300

    Fixed model post

--- post/models.py
+++ post/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
     )
-    text = (models.TextField(max_length=255),)
+    text = models.TextField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
