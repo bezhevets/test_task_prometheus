cc2d1ae4111a162bcee13f5ff684040042351384
Maksym Bezhevets <begik4@gmail.com>
2024-04-09 19:42:47 +0300

    Created app Post

---
+++ post/models.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.db import models
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+    )
+    text = (models.TextField(max_length=255),)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.text
