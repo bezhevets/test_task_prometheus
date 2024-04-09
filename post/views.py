f6e2925825cf6a4f542817c4f65cf940304a8ab1
Maksym Bezhevets <begik4@gmail.com>
2024-04-09 22:48:27 +0300

    Added permissions for PostViewSet

--- post/views.py
+++ post/views.py
@@ -1,13 +1,19 @@
 from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
 from post.models import Post
+from post.permissions import IsOwnerOrIfAuthenticatedReadOnly
 from post.serializers import PostSerializer, PostListSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (
+        IsAuthenticatedOrReadOnly,
+        IsOwnerOrIfAuthenticatedReadOnly,
+    )
 
     def get_serializer_class(self):
         if self.action == "list":
