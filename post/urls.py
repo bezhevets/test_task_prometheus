68d09b2c3f347e4230796566ade49349673ba3c7
Maksym Bezhevets <begik4@gmail.com>
2024-04-10 15:45:09 +0300

    Created analytics for likes, and added endpoint analytics.

--- post/urls.py
+++ post/urls.py
@@ -1,10 +1,15 @@
+from django.urls import path, include
 from rest_framework import routers
 
-from post.views import PostViewSet
+from post.views import PostViewSet, AnalyticsLikeView
 
 router = routers.DefaultRouter()
 router.register("posts", PostViewSet)
 
-urlpatterns = router.urls
+
+urlpatterns = [
+    path("analytics/", AnalyticsLikeView.as_view(), name="analytics"),
+    path("", include(router.urls)),
+]
 
 app_name = "post"
--- post/views.py
+++ post/views.py
@@ -1,7 +1,11 @@
+from datetime import datetime
+
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiResponse
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from post.models import Post, Like
 from post.permissions import IsOwnerOrIfAuthenticatedReadOnly
@@ -76,3 +80,67 @@ class PostViewSet(viewsets.ModelViewSet):
                 {"detail": "You've liked it."},
                 status=status.HTTP_200_OK,
             )
+
+
+@extend_schema(
+    parameters=[
+        OpenApiParameter(
+            name="date_from",
+            required=True,
+            description="Start date for analytics (YYYY-MM-DD)",
+            type=str,
+        ),
+        OpenApiParameter(
+            name="date_to",
+            required=True,
+            description="End date for analytics (YYYY-MM-DD)",
+            type=str,
+        ),
+    ],
+    responses={200: OpenApiResponse(description="Analytics data")},
+)
+class AnalyticsLikeView(APIView):
+    """Analytics by the number of likes by date"""
+
+    def get(self, request):
+        date_from = request.query_params.get("date_from")
+        date_to = request.query_params.get("date_to")
+
+        if date_to is None or date_from is None:
+            return Response(
+                {
+                    "detail": (
+                        "date_from and date_to are required parameters. "
+                        "Ex: /?date_from=2024-04-01&date_to=2024-04-11"
+                    )
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            date_from = datetime.strptime(date_from, "%Y-%m-%d").replace(
+                hour=0, minute=0, second=0
+            )
+            date_to = datetime.strptime(date_to, "%Y-%m-%d").replace(
+                hour=23, minute=59, second=59
+            )
+        except ValueError:
+            return Response(
+                {
+                    "error": "Incorrect format date. Ex: /?date_from=2024-04-01&date_to=2024-04-11"
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        likes_data = Like.objects.filter(created_at__range=[date_from, date_to])
+        likes_aggregated = {}
+
+        for like in likes_data:
+            date = like.created_at.date()
+            date = str(date)
+            if date in likes_aggregated:
+                likes_aggregated[date] += 1
+            else:
+                likes_aggregated[date] = 1
+
+        return Response(likes_aggregated, status=status.HTTP_200_OK)
