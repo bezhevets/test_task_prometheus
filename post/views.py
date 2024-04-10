4db959c5e21acf2e9f14fda903d976125d8bee66
Maksym Bezhevets <begik4@gmail.com>
2024-04-10 12:53:49 +0300

    Created endpoint like and func Like or unlike a post.

--- post/views.py
+++ post/views.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets, status
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
 
-from post.models import Post
+from post.models import Post, Like
 from post.permissions import IsOwnerOrIfAuthenticatedReadOnly
-from post.serializers import PostSerializer, PostListSerializer
+from post.serializers import PostSerializer, PostListSerializer, LikeSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -18,6 +19,8 @@ class PostViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action == "list":
             return PostListSerializer
+        if self.action == "like":
+            return LikeSerializer
         return PostSerializer
 
     def create(self, request, *args, **kwargs):
@@ -25,3 +28,30 @@ class PostViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save(owner=self.request.user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(
+        detail=True,
+        methods=["GET"],
+        url_path="like",
+        permission_classes=[IsAuthenticated],
+    )
+    def create_like(self, request, pk=None):
+        """Like or unlike a post."""
+        post = self.get_object()
+        user = self.request.user
+
+        likes = Like.objects.filter(owner=user, post=post)
+        if likes:
+            likes.delete()
+            return Response(
+                {"detail": "You have removed the like."},
+                status=status.HTTP_200_OK,
+            )
+        else:
+            serializer = LikeSerializer(data={"post": post.pk})
+            serializer.is_valid(raise_exception=True)
+            serializer.save(owner=user, post=post)
+            return Response(
+                {"detail": "You've liked it."},
+                status=status.HTTP_200_OK,
+            )
